84b46aac2a625407184a012a848677e64bc9dbcd
s1310031 <s1310031@u-aizu.ac.jp>
2024-07-02 16:24:15 +0900

    User name added

--- ex03_1.c
+++ ex03_1.c
@@ -2,6 +2,11 @@
 #include<stdlib.h>
 int main(){
   int a=0,b=0;
+  char name[100];
+  printf("Who are you?\n
+    ");
+  scanf("%s",name);
+  printf("Hello, %s!\n",)
   printf("Tossing a coin...\n");
   for(int i=1;i<=3;i++){
     if(rand()%2==1){
