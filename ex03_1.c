f0ed7b685f5d5d6f18fdb265a9de9126eb9a655f
s1310031 <s1310031@u-aizu.ac.jp>
2024-07-02 16:26:50 +0900

    Victory message added

--- ex03_1.c
+++ ex03_1.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 int main(){
   int a=0,b=0;
   printf("Tossing a coin...\n");
+  srand((unsigned int)time(NULL));
   for(int i=1;i<=3;i++){
     if(rand()%2==1){
     printf("Round %d: Heads\n",i);
@@ -13,4 +15,10 @@ int main(){
   }
   }
   printf("Heads: %d, Tails: %d\n",a,b);
+  if(a>b){
+    printf("%s won!\n",name);
+  }else{
+    printf("%s lose!\n",name);
+  }
+  return 0;
 }
